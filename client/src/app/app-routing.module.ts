b4ab355a82f031119ae1aa8baecd72a6116228ea
Nik <nikitama1hr@gmail.com>
2023-08-18 08:02:25 +0300

    initial structure

---
+++ client/src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import {NgModule} from '@angular/core';
+
+import {RouterModule, Routes} from '@angular/router';
+import {PostsComponent} from "./posts/posts.component";
+import {AuthComponent} from "./auth/auth.component";
+
+const routes: Routes = [
+  {path: '/posts', component: PostsComponent},
+  {path: '/auth', component: AuthComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {
+}
---
+++ client/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import {PostsComponent} from "./posts/posts.component";
+import {AuthComponent} from "./auth/auth.component";
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AuthComponent,
+    PostsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
